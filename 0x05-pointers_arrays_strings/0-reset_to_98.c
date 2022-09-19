a1a180ad0d4283867fc4654829eabae4a71a5b17
Dev-Pro001 <shekomwaza.devpro@gmail.com>
2022-09-19 12:57:57 -0700

    Debugged Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  *
  * *reset_to_98 - resets the value of given integer to 98
@@ -9,10 +11,13 @@
  * *Return : nothing
  *
  * */
+
+
+
 void reset_to_98(int *n)
 
 {
 
-	*n = 98;
+		*n = 98;
 
 }
