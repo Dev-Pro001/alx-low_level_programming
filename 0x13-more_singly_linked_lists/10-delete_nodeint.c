47ad8a42cb7cd468827b8756cebc43310dd57f8a
Dev-Pro001 <shekomwaza.devpro@gmail.com>
2022-10-26 21:00:43 +0100

    Updated Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,46 +1,40 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index of a listint_t list.
- * @head: pointer to the list.
- * @index: position of the node to delete.
- * Return: 1 if it succeeded, -1 if it failed.
- **/
+ * delete_nodeint_at_index - Deletes the node at a given
+ *                           index of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
+ *
+ * Return: On success - 1.
+ *         On failure - -1.
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *aux_node = *head;
-	listint_t *node_to_delete = *head;
-	unsigned int idx;
-	unsigned int cont = 0;
+	listint_t *tmp, *copy = *head;
+	unsigned int node;
 
-	/* border case for empty list */
-	if (!(*head))
+	if (copy == NULL)
 		return (-1);
 
-	/* border case for delete at the beginning */
 	if (index == 0)
 	{
-		*head = node_to_delete->next;
-		free(node_to_delete);
+		*head = (*head)->next;
+		free(copy);
 		return (1);
 	}
 
-	/* search of position to delete */
-	idx = index - 1;
-	while (aux_node && cont != idx)
+	for (node = 0; node < (index - 1); node++)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		if (copy->next == NULL)
+			return (-1);
+
+		copy = copy->next;
 	}
 
-	/* general case */
-	if (cont == idx && aux_node)
-	{
-		node_to_delete = aux_node->next;
-		aux_node->next = node_to_delete->next;
-		free(node_to_delete);
-		return (1);
-	}
-
-	return (-1);
+	tmp = copy->next;
+	copy->next = tmp->next;
+	free(tmp);
+	return (1);
 }
